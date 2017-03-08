bf68c96d326bf91df042a6d517e7b36366f81777
Hanul <hanul@hanul.me>
2017-03-08 21:25:44 +0900

    ES6 반영

--- SkyMongoBackup.js
+++ SkyMongoBackup.js
@@ -1,90 +1,64 @@
-require('./UJS-NODE.js');
+require('uppercase-core');
 
-RUN(function() {
+let exec = require('child_process').exec;
 
-	var
-	//IMPORT: exec
-	exec = require('child_process').exec,
+let config = JSON.parse(READ_FILE({
+	path : 'config.json',
+	isSync : true
+}));
+
+let configs = config.configs === undefined ? [config] : config.configs;
+
+// 1분에 한번씩 체크
+INTERVAL(60, RAR(() => {
 	
-	// config
-	config = JSON.parse(READ_FILE({
-		path : 'config.json',
-		isSync : true
-	})),
+	let nowCal = CALENDAR();
 	
-	// configs
-	configs = config.configs === undefined ? [config] : config.configs;
+	let todayStr = nowCal.getYear() + '-' + nowCal.getMonth(true) + '-' + nowCal.getDate(true);
 	
-	// 1분에 한번씩 체크
-	INTERVAL(60, RAR(function() {
+	// 새벅 5시에 백업
+	if (nowCal.getHour() === 5 && nowCal.getMinute() === 0) {
 		
-		var
-		// now cal
-		nowCal = CALENDAR(),
-		
-		// today str
-		todayStr = nowCal.getYear() + '-' + (nowCal.getMonth() < 10 ? '0' + nowCal.getMonth() : nowCal.getMonth()) + '-' + (nowCal.getDate() < 10 ? '0' + nowCal.getDate() : nowCal.getDate()),
-		
-		// last week cal
-		lastWeekCal,
-		
-		// last week str
-		lastWeekStr;
-		
-		// 새벅 5시에 백업
-		if (nowCal.getHour() === 5 && nowCal.getMinute() === 0) {
+		EACH(configs, (config) => {
 			
-			EACH(configs, function(config) {
-				
-				var
-				// db name
-				dbName = config.dbName,
-				
-				// db host
-				dbHost = config.dbHost === undefined ? '127.0.0.1' : config.dbHost,
+			let dbName = config.dbName;
+			let dbHost = config.dbHost === undefined ? '127.0.0.1' : config.dbHost;
+			let dbPort = config.dbPort === undefined ? 27017 : config.dbPort;
+			let dbUsername = config.dbUsername;
+			let dbPassword = config.dbPassword;
 			
-				// db port
-				dbPort = config.dbPort === undefined ? 27017 : config.dbPort,
-				
-				// db username
-				dbUsername = config.dbUsername,
-				
-				// db password
-				dbPassword = config.dbPassword,
-				
-				// command
-				command = 'mongodump';
-				
-				command += ' --host ' + dbHost;
-				command += ' --port ' + dbPort;
-				command += ' --db ' + dbName;
-				
-				if (dbUsername !== undefined) {
-					command += ' --username ' + dbUsername;
+			// command
+			command = 'mongodump';
+			
+			command += ' --host ' + dbHost;
+			command += ' --port ' + dbPort;
+			command += ' --db ' + dbName;
+			
+			if (dbUsername !== undefined) {
+				command += ' --username ' + dbUsername;
+			}
+			
+			if (dbPassword !== undefined) {
+				command += ' --password ' + dbPassword;
+			}
+			
+			command += ' --out ' + __dirname + '/__BACKUP/' + todayStr;
+			
+			exec(command, (error) => {
+				if (error === TO_DELETE) {
+					console.log(CONSOLE_GREEN(todayStr + ' [SkyMongoBackup-' + dbName + '] 백업하였습니다.'));
+				} else {
+					SHOW_ERROR('SkyMongoBackup-' + dbName, error.toString());
 				}
-				
-				if (dbPassword !== undefined) {
-					command += ' --password ' + dbPassword;
-				}
-				
-				command += ' --out ' + __dirname + '/__BACKUP/' + todayStr;
-				
-				exec(command, function(error) {
-					if (error === TO_DELETE) {
-						console.log(CONSOLE_GREEN('[SkyMongoBackup] ' + todayStr + ' `' + dbName + '` BACKED UP!'));
-					} else {
-						console.log(CONSOLE_RED('[SkyMongoBackup] `' + dbName + '` ERROR:'), error.toString());
-					}
-				});
 			});
-			
-			lastWeekCal = CALENDAR(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000));
-			lastWeekStr = lastWeekCal.getYear() + '-' + (lastWeekCal.getMonth() < 10 ? '0' + lastWeekCal.getMonth() : lastWeekCal.getMonth()) + '-' + (lastWeekCal.getDate() < 10 ? '0' + lastWeekCal.getDate() : lastWeekCal.getDate());
-			
-			// 7일 전 껀 삭제
-			REMOVE_FOLDER(__dirname + '/__BACKUP/' + lastWeekStr);
-		}
-	}));
-	
-	console.log('[SkyMongoBackup] RUNNING...');
-});
+		});
+		
+		let lastDayCal = CALENDAR(new Date(Date.now() - 1 * 24 * 60 * 60 * 1000));
+		let lastDayStr = lastDayCal.getYear() + '-' + lastDayCal.getMonth(true) + '-' + lastDayCal.getDate(true);
+		
+		// 어제껀 삭제
+		REMOVE_FOLDER(__dirname + '/__BACKUP/' + lastDayStr);
+	}
+}));
+
+console.log('[SkyMongoBackup] 실행중입니다.');
