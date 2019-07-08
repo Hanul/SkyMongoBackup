6b7153fdcaf38e2301e729fdae9963495db0fb86
Hanul <hanul@hanul.me>
2019-07-08 16:30:20 +0900

    Collection 백업 기능 추가

--- README.md
+++ README.md
@@ -9,10 +9,15 @@
 	"dbHost": "127.0.0.1",
 	"dbPort": 27017,
 	"dbUsername": "test",
-	"dbPassword": "1234"
+	"dbPassword": "1234",
+	"collections": [
+		"MyProject.User",
+		"MyProject.User__History"
+	]
 }
 ```
 * `dbName`는 필수이며, 나머지 설정들은 선택입니다.
+* `collections`를 설정하게 되면 모든 데이터가 아닌 지정한 Collection들의 데이터만 백업하게 됩니다.
 
 ## 실행
 ```
--- SkyMongoBackup.js
+++ SkyMongoBackup.js
@@ -33,65 +33,101 @@ let checkIsAllowedFolderName = (name) => {
 	);
 };
 
-if (config.dbName !== undefined) {
+// 1분에 한번씩 체크
+INTERVAL(60, RAR(() => {
 	
-	// 1분에 한번씩 체크
-	INTERVAL(60, RAR(() => {
+	let nowCal = CALENDAR();
+	
+	let todayStr = nowCal.getYear() + '-' + nowCal.getMonth(true) + '-' + nowCal.getDate(true);
+	
+	// 새벅 5시에 백업
+	if (nowCal.getHour() === 5 && nowCal.getMinute() === 0) {
 		
-		let nowCal = CALENDAR();
-		
-		let todayStr = nowCal.getYear() + '-' + nowCal.getMonth(true) + '-' + nowCal.getDate(true);
-		
-		// 새벅 5시에 백업
-		if (nowCal.getHour() === 5 && nowCal.getMinute() === 0) {
+		EACH(configs, (config) => {
 			
-			EACH(configs, (config) => {
+			if (config.dbName !== undefined) {
 				
 				let dbName = config.dbName;
 				let dbHost = config.dbHost === undefined ? '127.0.0.1' : config.dbHost;
 				let dbPort = config.dbPort === undefined ? 27017 : config.dbPort;
 				let dbUsername = config.dbUsername;
 				let dbPassword = config.dbPassword;
+				let collections = config.collections;
 				
-				let backupServerHost = config.backupServerHost;
-				let backupServerPort = config.backupServerPort;
-				
-				// command
-				command = 'mongodump';
-				
-				command += ' --host ' + dbHost;
-				command += ' --port ' + dbPort;
-				command += ' --db ' + dbName;
-				
-				if (dbUsername !== undefined) {
-					command += ' --username ' + dbUsername;
+				if (collections !== undefined) {
+					
+					EACH(collections, (collection) => {
+						
+						// command
+						let command = 'mongodump';
+						
+						command += ' --host ' + dbHost;
+						command += ' --port ' + dbPort;
+						command += ' --db ' + dbName;
+						command += ' --collection ' + collection;
+						
+						if (dbUsername !== undefined) {
+							command += ' --username ' + dbUsername;
+						}
+						
+						if (dbPassword !== undefined) {
+							command += ' --password ' + dbPassword;
+						}
+						
+						command += ' --out ' + __dirname + '/__BACKUP/' + todayStr;
+						
+						exec(command, (error) => {
+							
+							if (error === TO_DELETE) {
+								console.log(CONSOLE_GREEN(todayStr + ' [SkyMongoBackup ' + dbName + ' ' + collection + '] 백업하였습니다.'));
+							}
+							
+							else {
+								SHOW_ERROR('SkyMongoBackup ' + dbName + ' ' + collection, error.toString());
+							}
+						});
+					});
 				}
 				
-				if (dbPassword !== undefined) {
-					command += ' --password ' + dbPassword;
+				else {
+					
+					// command
+					let command = 'mongodump';
+					
+					command += ' --host ' + dbHost;
+					command += ' --port ' + dbPort;
+					command += ' --db ' + dbName;
+					
+					if (dbUsername !== undefined) {
+						command += ' --username ' + dbUsername;
+					}
+					
+					if (dbPassword !== undefined) {
+						command += ' --password ' + dbPassword;
+					}
+					
+					command += ' --out ' + __dirname + '/__BACKUP/' + todayStr;
+					
+					exec(command, (error) => {
+						
+						if (error === TO_DELETE) {
+							console.log(CONSOLE_GREEN(todayStr + ' [SkyMongoBackup ' + dbName + '] 백업하였습니다.'));
+						}
+						
+						else {
+							SHOW_ERROR('SkyMongoBackup ' + dbName, error.toString());
+						}
+					});
 				}
-				
-				command += ' --out ' + __dirname + '/__BACKUP/' + todayStr;
-				
-				exec(command, (error) => {
-					
-					if (error === TO_DELETE) {
-						console.log(CONSOLE_GREEN(todayStr + ' [SkyMongoBackup-' + dbName + '] 백업하였습니다.'));
-					}
-					
-					else {
-						SHOW_ERROR('SkyMongoBackup-' + dbName, error.toString());
-					}
-				});
-			});
-			
-			let lastDayCal = CALENDAR(new Date(Date.now() - 1 * 24 * 60 * 60 * 1000));
-			let lastDayStr = lastDayCal.getYear() + '-' + lastDayCal.getMonth(true) + '-' + lastDayCal.getDate(true);
-			
-			// 어제껀 삭제
-			REMOVE_FOLDER(__dirname + '/__BACKUP/' + lastDayStr);
-		}
-	}));
-}
+			}
+		});
+		
+		let lastDayCal = CALENDAR(new Date(Date.now() - 1 * 24 * 60 * 60 * 1000));
+		let lastDayStr = lastDayCal.getYear() + '-' + lastDayCal.getMonth(true) + '-' + lastDayCal.getDate(true);
+		
+		// 어제껀 삭제
+		REMOVE_FOLDER(__dirname + '/__BACKUP/' + lastDayStr);
+	}
+}));
 
 console.log('[SkyMongoBackup] 실행중입니다.');
